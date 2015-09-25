fcb2051b310083d234f6ad6742444a102121985b
Tunghsiao Liu <t@sparanoid.com>
2015-09-25 18:05:46 +0800

    chore: release 1.0.5

--- slimpack.php
+++ slimpack.php
@@ -3,7 +3,7 @@
 Plugin Name: Slimpack
 Plugin URI: http://sparanoid.com/work/slimpack/
 Description: Slimpack — Lightweight Jetpack. Super-fast performance without modules that require contracting WordPress.com.
-Version: 1.0.4
+Version: 1.0.5
 Author: Tunghsiao Liu
 Author URI: http://sparanoid.com/
 Author Email: t@sparanoid.com
